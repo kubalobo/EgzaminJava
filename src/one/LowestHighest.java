cb9486c37aeb66b5fb9759b427fbdafc04781a84
Kuba <kubalobo@gmail.com>
2017-06-17 19:56:46 +0200

    Initial commit.

---
+++ src/one/LowestHighest.java
@@ -0,0 +1,31 @@
+package one;
+
+import java.util.Arrays;
+
+/**
+ * Created by Kuba on 17.06.2017.
+ * Napisz program wczytujący 3 liczby całkowite, który wypisze najmniejszą i największą z nich.
+ */
+class LowestHighest {
+    private int[] numbers = new int[3];
+
+    LowestHighest(int a, int b, int c) {
+        numbers = new int[] {a, b, c};
+    }
+
+    void sortSolution() {
+        Arrays.sort(numbers);
+        System.out.print(numbers[0] + " " + numbers[2]);
+    }
+
+    void compareSolution() {
+        int min = 99999, max = -99999;
+        for (int number : numbers) {
+            if (min > number)
+                min = number;
+            if (max < number)
+                max = number;
+        }
+        System.out.print(min + " " + max);
+    }
+}
---
+++ tests/one/LowestHighestTest.java
@@ -0,0 +1,41 @@
+package one;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Kuba on 17.06.2017.
+ * Test of the first exercise implementation.
+ */
+public class LowestHighestTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUp() throws Exception {
+        System.setOut(new PrintStream(outContent)); // Capture the system out stream to test results.
+    }
+
+    @Test
+    public void testSortSolution() {
+        LowestHighest LW = new LowestHighest(2, 3 ,1);
+        LW.sortSolution();
+        assertEquals("1 3", outContent.toString());
+        System.setOut(null);
+    }
+
+    @Test
+    public void testCompareSolution() {
+        LowestHighest LW = new LowestHighest(2, 3 ,1);
+        LW.compareSolution();
+        assertEquals("1 3", outContent.toString());
+        System.setOut(null);
+    }
+
+}
