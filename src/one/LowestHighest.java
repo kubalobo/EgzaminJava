fe4fb5eda3f0a8ddfac68a8ba4d80d74b68fc2a6
Kuba <kubalobo@gmail.com>
2017-06-18 11:40:31 +0200

    Task two - name. Classes and methods are now public and it is not showed as error in inspection.

--- src/one/LowestHighest.java
+++ src/one/LowestHighest.java
@@ -6,19 +6,19 @@ import java.util.Arrays;
  * Created by Kuba on 17.06.2017.
  * Napisz program wczytujący 3 liczby całkowite, który wypisze najmniejszą i największą z nich.
  */
-class LowestHighest {
+public class LowestHighest {
     private int[] numbers = new int[3];
 
     LowestHighest(int a, int b, int c) {
         numbers = new int[] {a, b, c};
     }
 
-    void sortSolution() {
+    public void sortSolution() {
         Arrays.sort(numbers);
         System.out.print(numbers[0] + " " + numbers[2]);
     }
 
-    void compareSolution() {
+    public void compareSolution() {
         int min = 99999, max = -99999;
         for (int number : numbers) {
             if (min > number)
--- src/two/Tax.java
+++ src/two/Tax.java
@@ -4,7 +4,10 @@ import java.util.Scanner;
 
 /**
  * Created by Kuba on 17.06.2017.
- *
+ *Napisz program obliczający należny podatek dochodowy. Program pobiera od użytkownika
+ dochód, następnie oblicza podatek wg skali:
+ - dochód do 85528 zł – 18% minus kwota 556,02 zł,
+ - dochód powyżej 85528 zł – 15395,04 zł plus 32% nadwyżki ponad 85528 zł.
  */
 public class Tax {
     public void taxCalculator() {
