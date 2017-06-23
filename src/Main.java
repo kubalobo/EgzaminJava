dda5abd4cfa2c98f54fe52de06815e22cf525cc3
Kuba <kubalobo@gmail.com>
2017-06-23 08:43:46 +0200

    List dir drugi sposob

--- src/Main.java
+++ src/Main.java
@@ -18,6 +18,6 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Dir.list("C:");
+        Dir.list2("C:");
     }
 }
--- src/_11/Dir.java
+++ src/_11/Dir.java
@@ -1,7 +1,11 @@
 package _11;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * Created by Kuba on 22.06.2017.
@@ -24,21 +28,22 @@ public class Dir {
                 System.out.println("Directory " + file.getName() );
             }
         }
-
     }
-    /*public static void list2(String path) {
-        Files folder = new Files(path);
-        File[] listOfFiles = folder.listFiles();
-
-        for (File file : listOfFiles) {
-            if (file.isFile()) {
-                System.out.println("File " + file.getName() +
-                        " Size: " + file.getTotalSpace() +
-                        " Date: " + file.);
-            } else if (file.isDirectory()) {
-                System.out.println("Directory " + file.getName() );
+    public static void list2(String directory) {
+        try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(Paths.get(directory))) {
+            for(Path path : directoryStream) {
+                System.out.println(
+                        path.getFileName() +
+                                " Rozmiar: " + Files.size(path) +
+                                " Modyfikacja: " + Files.getLastModifiedTime(path).toString() +
+                                " Prawa: " + "exe: " + Files.isExecutable(path) +
+                                " read: " + Files.isReadable(path) +
+                                " writable: " + Files.isWritable(path));
             }
-        }
 
-    }*/
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
